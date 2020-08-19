3804644f0242b317b118b827dfa53e324322e623
Matheus Carregal <matheuschabib@gmail.com>
2020-08-19 14:49:59 -0300

    Update main.js console.log

--- js/main.js
+++ js/main.js
@@ -1,6 +1,12 @@
 var nome = "Matheus Carregal";
 var idade = 18;
 var idade2 = 10;
+var frase = "Japão é o melhor time do mundo";
 //alert("Bem vindo " + nome);
 //alert (nome +" tem " + idade + " anos.");
-alert(idade + idade2);
+//alert(idade + idade2);
+console.log(nome);
+console.log(idade + idade2);
+console.log(frase.replace("Japão","Brasil"));
+//alert(frase.replace("Japão","Brasil"));
+console.log(frase.toLowerCase());
