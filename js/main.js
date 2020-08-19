7894c0c0478088bda3c523a6da2de9ca3dfbaa43
Matheus Carregal <matheuschabib@gmail.com>
2020-08-19 14:41:27 -0300

    Update main.js New alerts

--- js/main.js
+++ js/main.js
@@ -1,1 +1,6 @@
-alert("meu primeiro js");
+var nome = "Matheus Carregal";
+var idade = 18;
+var idade2 = 10;
+//alert("Bem vindo " + nome);
+//alert (nome +" tem " + idade + " anos.");
+alert(idade + idade2);
